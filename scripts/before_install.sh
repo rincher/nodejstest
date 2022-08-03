0f786d3aa6e6efa3c209cecc94f19fbeefd2e4a5
rincher <pillarofthelight@gmail.com>
2022-08-03 17:07:00 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,13 +5,10 @@ sudo yum update
 
 sudo yum install curl -y
 
-sudo curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh -o install_nvm.sh
+sudo curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-bash install_nvm.sh
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 sudo nvm install --lts
 # Install nodemon
