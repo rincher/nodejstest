767ab1036190330c3e7bf94e45e4377bb59e19ce
rincher <pillarofthelight@gmail.com>
2022-08-03 17:35:18 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install node.js
-sudo yum update
+yum update
 
-sudo yum install curl -y
+yum install curl -y
 
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export HOME="/home/ec2-user/"
@@ -12,10 +12,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-sudo nvm install --lts
+nvm install --lts
 # Install nodemon
-sudo npm install nodemon -g
+npm install nodemon -g
 
 # Install forever module 
 # https://www.npmjs.com/package/forever
-sudo npm install forever -g
+npm install forever -g
