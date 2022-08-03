67c1a293d32ec3732a934dd0e35cf4772061ce1e
rincher <pillarofthelight@gmail.com>
2022-08-03 16:35:37 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,6 +2,14 @@
 
 # Install node.js
 sudo yum update
+curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh -o install_nvm.sh
+bash install_nvm.sh
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+# Install NPM
+
 
 # Install nodemon
 sudo npm install nodemon -g
