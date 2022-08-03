8b413ebbcac7973aca2624a0a58fb1fed817af7c
rincher <pillarofthelight@gmail.com>
2022-08-03 16:56:24 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -11,6 +11,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+bash install_nvm.sh
+
 sudo nvm install --lts
 # Install nodemon
 sudo npm install nodemon -g
