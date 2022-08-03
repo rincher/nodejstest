96b963de7d048161c8558746abea930bb8fb45ec
rincher <pillarofthelight@gmail.com>
2022-08-03 17:49:26 +0900

    test

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+rm -rf /home/ec2-user/nodejs
 mkdir /home/ec2-user/nodejs
 cd /home/ec2-user/nodejs
 
