e7e428fa655946861a34849eb21bcc6ea80bc81e
rincher <pillarofthelight@gmail.com>
2022-08-03 17:09:49 +0900

    test

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,7 +5,7 @@ sudo yum update
 
 sudo yum install curl -y
 
-sudo curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
