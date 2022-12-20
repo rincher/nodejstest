a39c0f60384acdac159ab88a222244ab25025371
rincher <pillarofthelight@gmail.com>
2022-12-20 17:49:12 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,14 +5,14 @@ sudo yum update
 
 sudo yum install curl -y
 
+rm -rf $NVM_DIR ~/.npm ~/.bower
+
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash install_nvm.sh
 export HOME="/home/ec2-user/"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-rm -rf $NVM_DIR ~/.npm ~/.bower
 
 #nvm install 16.15.1
 
