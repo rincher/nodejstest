a80848ef04819f8c26dcd05824c3442fa598efc9
rincher <pillarofthelight@gmail.com>
2022-12-20 14:20:00 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install node.js
-yum update
+sudo yum update
 
-yum install curl -y
+sudo yum install curl -y
 
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export HOME="/home/ec2-user/"
@@ -13,6 +13,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 rm -rf $NVM_DIR ~/.npm ~/.bower
+
+# nvm install 16.15.1
 
 # Install nodemon
 npm install nodemon -g
