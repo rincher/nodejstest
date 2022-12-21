6fdda737706fb4dac6c6804f29501cc223716b8c
rincher <pillarofthelight@gmail.com>
2022-12-21 14:33:30 +0900

    Update BeforeInstall.sh

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -16,7 +16,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-sudo rm -rf "$HOME/nodejs*"
+sudo rm -rf $HOME/nodejs*
 
 nvm install 16.15.1
 
