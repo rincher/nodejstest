6a7e683113b3a57536fa20703bdffb8e35eea5fe
rincher <pillarofthelight@gmail.com>
2022-12-21 14:17:55 +0900

    script edited

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -6,5 +6,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Stop all servers and start the server as a daemon
-# forever stopall
-# forever start /home/ubuntu/nodejs/app.js
+forever stopall
+forever start /home/ec2-user/nodejs/app.js
